98f51ece89b9ca47b43ae09816e13515d44728b4
Ferrusia <ziminior18@gmail.com>
2026-08-02 14:17:51 +0700

    Quit Button

--- scenes/Main_Menu/menu.gd
+++ scenes/Main_Menu/menu.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Game_Scene/main_game.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
