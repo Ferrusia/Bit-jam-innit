fd93fc4bdba035d869273753d56c63d23076f808
Ferrusia <ziminior18@gmail.com>
2026-08-03 19:58:34 +0700

    sfx player

--- globals/GameManager.gd
+++ globals/GameManager.gd
@@ -2,3 +2,13 @@ extends Node
 
 var Breath_count : float = 100
 var Is_Breathing : bool = false
+
+var Night : int = 1
+var SleepyCount : int = 0
+
+func PSFX(stream: AudioStream):
+	var asp = AudioStreamPlayer.new()
+	add_child(asp)
+	asp.stream = stream
+	asp.finished.connect(asp.queue_free) # Delete the node when done
+	asp.play()
