5ffedf01440974911539518697446e1bb2c42b43
Ferrusia <ziminior18@gmail.com>
2026-08-05 17:31:37 +0700

    Feet Tickler

--- characters/Feet_Tickler/feet_tickler.gd
+++ characters/Feet_Tickler/feet_tickler.gd
@@ -1,6 +1,17 @@
 extends Node3D
 
-var Speed : float = 100
+var Speed : float = 0.05
 var Origin : Vector3 = global_position
+var Dir : int = 1
+
 func _process(delta: float) -> void:
-	position.y -= Speed * delta # TODO : FIX TS
+	position.y += Speed * delta * Dir# TODO : FIX TS
+	if Input.is_action_pressed("Breathe"):
+		Dir = -1.25
+	else:
+		Dir = 1.25
+	if global_position.y >= 1.5:
+		global_position = Origin
+		GameManager.Attack_Player("Feet_Tickler", 25)
+		
+		GameManager.PSFX(preload("res://sound_library/Minecraft cave noises 4.mp3"))
